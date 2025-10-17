7993c67a655992efe1ff116502e94c775c0c2bff
JuanBCl <est.juanj.bernal@unimilitar.edu.co>
2025-10-17 09:48:49 -0500

    deploy page web

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/safirax-prototipo/', // 👈 pon aquí el nombre de tu repo
   plugins: [react()],
 })
